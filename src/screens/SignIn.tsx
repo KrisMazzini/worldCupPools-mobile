fc011bf78dd92b3f6c8cec3bc386b45d904f9f7c
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-11-02 12:18:17 -0300

    create sign in context

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,29 +1,32 @@
 import { Center, Text, Icon } from 'native-base'
-import { Fontisto } from '@expo/vector-icons' 
+import { Fontisto } from '@expo/vector-icons'
+import { useAuth } from '../hooks/useAuth'
 
 import Logo from '../assets/logo.svg'
 import { Button } from '../components/Button'
 
 export function SignIn() {
+    const { signIn } = useAuth()
+
     return (
-    <Center flex={1} bgColor="gray.900" p={7}>
-        <Logo width={212} height={40} />
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40} />
 
-        <Button 
-            type="SECONDARY"
-            title="ENTRAR COM GOOGLE"
-            mt={12}
-            leftIcon={
-                <Icon as={Fontisto} name="google" color="white" size="md" />
-            }
-        />
+            <Button 
+                type="SECONDARY"
+                title="ENTRAR COM GOOGLE"
+                mt={12}
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                onPress={signIn}
+            />
 
-        <Text
-            color="white"
-            textAlign="center"
-            mt={4}
-        >
-            Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
-        </Text>
-    </Center>)
+            <Text
+                color="white"
+                textAlign="center"
+                mt={4}
+            >
+                Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+            </Text>
+        </Center>
+    )
 }
